b737bd3d64046267b0c575b54c37de470e0c9f84
Tyler Finley & Vaughn Dice <student@epicodus.com>
2015-08-17 16:23:10 -0700

    add integration spec testing new task form

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -31,7 +31,7 @@ describe('The To Do List Manager App', {:type => :feature}) do
     end
   end
 
-  describe('viewing spesific list') do
+  describe('viewing specific list') do
     it("allows a user to view a specific list") do
       list = List.new({:name => 'Test List', :id => nil})
       list.save()
@@ -39,9 +39,7 @@ describe('The To Do List Manager App', {:type => :feature}) do
       click_link('Test List')
       expect(page).to have_content(list.name)
     end
-  end
-  describe('create new task in a specific list') do
-    it('allows user to create task in a list') do
+    it('allows user to view task(s) in a list') do
       list = List.new({:name => 'whatever', :id => nil})
       list.save()
       task = Task.new({:description => 'test', :list_id => list.id, :due_date => Date.new(2015,9,18)})
@@ -49,6 +47,20 @@ describe('The To Do List Manager App', {:type => :feature}) do
       visit("/lists/#{list.id}")
       expect(page).to have_content(task.description)
       expect(page).to have_content(task.due_date)
+    end
+  end
+
+  describe('creating a new task') do
+    it('allows user to create a new task for a list') do
+      list = List.new({:name => 'whatever', :id => nil})
+      list.save()
+      visit("/lists/#{list.id}")
+      click_link('Enter a task')
+      fill_in('task_description', :with => 'new task description')
+      fill_in('due_date', :with => Date.new(2015,9,18))
+      click_button('Add Task')
+      expect(page).to have_content('new task description')
+      expect(page).to have_content(Date.new(2015,9,18))
     end
   end
 end
